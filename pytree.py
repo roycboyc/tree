47135631fd72f1d971ef0d66635e0d972bd9c748
roycboyc <rc726@cornell.edu>
2016-10-05 08:49:08 -0400

    changes more

--- pytree.py
+++ pytree.py
@@ -11,12 +11,12 @@ space_sign = "    "
 # https://www.tutorialspoint.com/python/os_listdir.htm
 def tree_print(path, shape, count):
     dir_list = os.listdir(path)
-           
-    # remove files or directories
-    dir_list = list(filter(lambda key: not key.startswith('.'), dir_list))
-    dir_list = sorted(dir_list, key = lambda x: x.lower())   # removed key = sub_me to test
 
-    # iterate over all of the files and directories in this one
+    # remove and sort
+    #dir_list = [dir_list for dir_list in dir_list if dir_list[0] != '.']
+    dir_list = sorted(dir_list) #, key = lambda x: x.basename.lower())  
+
+    # iterate over all of the files and directories
     for file_name in dir_list:
 
         # extract the path name for this file in prep for recursion
@@ -31,11 +31,11 @@ def tree_print(path, shape, count):
             next_shape = unicode_line   # the line(s) will be printed once the next file in this folder is called
         # if this is a directory, count it in the dict and apply the function to it
         if os.path.isdir(full_path):
-            count['directories'] += 1
+            count['directories'] = count['directories'] + 1
             tree_print(file_name, shape + next_shape, count)
         # if not a dir, just count files for final printout
         else:   
-        	count['files'] += 1
+        	count['files'] = count['files'] + 1
     
 if __name__ == '__main__':
     count = {'directories': 0, 'files': 0}
@@ -48,11 +48,10 @@ if __name__ == '__main__':
     elif len(sys.argv) == 1:
     	sys_path = '.'
     elif len(sys.argv) == 0:
-    	print('idk')
     	sys.exit(0)
 
-    print (sys_path)
+    print(sys_path)
     tree_print(sys_path, '', count)
 
     print
-    print("%s directories, %s files" (count['directories'], count['files']))
+    print("%s directories, %s files" % (count['directories'], count['files']))
