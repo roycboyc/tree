58de54d550063493654b27f453565762da545f46
roycboyc <rc726@cornell.edu>
2016-10-05 07:01:38 -0400

    minimal sorting

--- pytree.py
+++ pytree.py
@@ -14,22 +14,21 @@ def tree_print(path, shape, count):
        
     # remove files or directories
     dir_list = list(filter (lambda key: not key.startswith('.'), dir_list))
-    dir_list = sorted (dir_list, key=sub_me)
+    dir_list = sorted(dir_list) # removed key = sub_me to test
 
     # iterate over all of the files and directories in this one
     for file_name in dir_list:
 
         # extract the path name for this file in prep for recursion
-        full_path = path + '/' + file_name
+        full_path = str(path) + '/' + str(file_name)
 
-        # if this is the last file (an index loop might be more sensible? had bugs with that)
+        # if this is the last file 
         if file_name == dir_list[len(files) - 1]:
             print(shape + unicode_end + file_name)
             next_shape = space_sign
-
-		else: # if not, print the cont_shape and assign the next shape as line
+        else: # if not, print the cont_shape and assign the next shape as line
             print(shape + unicode_cont, file_name)
-            next_shape = unicode_line
+            next_shape = unicode_line # the line(s) will be printed once the next file in this folder is called
                        
 		if os.path.indir(full_path): # if this is a directory, count it in the dict and apply the function to it
             count['directories'] += 1
@@ -38,33 +37,19 @@ def tree_print(path, shape, count):
 		else: # if not a dir, just count files for final printout
             count['files'] += 1
 
-# this function helps the sorting in the tree_print function    
-def sub_me(dir_name):
-    # if the first character is a letter, return it in lower case
-    if dir_name[0].isalpha:
-        return dir_name.lower()
-    # if it's not, find the first character that is, and return it lower case
-    else:
-        for i in range(len(dir_name)):
-            if dir_name[i].isalpha():
-                former = dir_name[:i]
-                rest_of = dir_name[i:].lower()
-                ret = former + rest_of
-                return ret
-            else:
-                continue
                 
 if __name__ == '__main__':
     count = {'directories': 0, 'files': 0}
     
-    if len(sys.argv == 1):
-    	sys_path = '.'
+    if len(sus.argv>2):
+    	print ('invalid path')
     elif len(sys.argv == 2):
 		sys_path = sys.argv[1]
-    else:
-        print('invalid path')
-        sys.exit(0)
-        
+    elif len(sys.argv == 1):
+    	sys_path = '.'
+    elif len(sys.argv == 0):
+    	print ('empty path')
+    	
     print (sys_path)
     tree_print(sys_path, "", count)
     
